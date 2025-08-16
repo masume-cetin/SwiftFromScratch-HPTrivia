021956732f1ec51be8a4f0c82d9a45edf1092bee
Masume <cetinmasume@gmail.com>
2025-08-16 15:55:43 +0300

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by masume çetin on 16.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
