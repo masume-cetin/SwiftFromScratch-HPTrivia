0896a6e4a6cd76162c36216061a06964cecb1a9f
Masume <cetinmasume@gmail.com>
2025-08-18 15:45:32 +0300

    update : gameplay screen controls added - dispatch queue

--- HPTrivia/Views/ContentView.swift
+++ HPTrivia/Views/ContentView.swift
@@ -49,7 +49,7 @@ struct ContentView: View {
         let sound = Bundle.main.path(forResource:"magic-in-the-air",ofType:"mp3")
         audioPlayer = try! AVAudioPlayer(contentsOf:URL(filePath: sound!))
         audioPlayer.numberOfLoops = -1
-      //  audioPlayer.play()
+        audioPlayer.play()
     }
 }
 
--- HPTrivia/Views/GamePlay.swift
+++ HPTrivia/Views/GamePlay.swift
@@ -6,8 +6,14 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct GamePlay: View {
+    @State private var musicPlayer : AVAudioPlayer!
+    @State private var sfxPlayer : AVAudioPlayer!
+    @State private var animateViewsIn :Bool = false
+    @Environment(Game.self) private var game
+    @Environment(\.dismiss) private var dismiss
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -20,23 +26,78 @@ struct GamePlay: View {
                     }
                 VStack {
                     // MARK: controls
+                    HStack{
+                        Button("End Game"){
+                            game.endGame()
+                            dismiss()
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(.red.opacity(0.5))
+                        Spacer()
+                        Text("Score : \(game.gameScore)")
+                    }
+                    .padding()
+                    .padding(.vertical,30)
                     
                     // MARK: question
-                    
+                    VStack{
+                    if animateViewsIn {
+                        Text(game.currentQuestion.question)
+                            .font(.custom("PartyLetPlain", size: 50))
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .transition(.scale)
+                    } }
+                    .animation(.easeInOut(duration: 2),value: animateViewsIn)
+                    Spacer()
                     // MARK: hints
                     
                     // MARK: answers
                 }
                 .frame(width: geo.size.width,height: geo.size.height)
-                
+                .foregroundStyle(.white)
                 // MARK: celebration
             }
             .frame(width: geo.size.width,height: geo.size.height)
         }
         .ignoresSafeArea()
+        .onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+            animateViewsIn = true
+            }
+            game.startGame()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+            playMusic()
+            }
+        }
+    }
+    private func playMusic() {
+        let songs = ["let-the-mistery-unfold","spellcraft","hiding-place-in-the-forest","deep-in-the-dell"]
+        let song = songs.randomElement()!
+        let sound = Bundle.main.path(forResource:song,ofType:"mp3")
+        musicPlayer = try! AVAudioPlayer(contentsOf:URL(filePath: sound!))
+        musicPlayer.numberOfLoops = -1
+        musicPlayer.volume = 0.1
+        musicPlayer.play()
+    }
+    private func playFlipSound() {
+        let sound = Bundle.main.path(forResource:"page-flip",ofType:"mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf:URL(filePath: sound!))
+        sfxPlayer.play()
+    }
+    private func playWrongSound() {
+        let sound = Bundle.main.path(forResource:"negative-beeps",ofType:"mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf:URL(filePath: sound!))
+       sfxPlayer.play()
+    }
+    private func playCorrectSound() {
+        let sound = Bundle.main.path(forResource:"magic-wand",ofType:"mp3")
+        sfxPlayer = try! AVAudioPlayer(contentsOf:URL(filePath: sound!))
+        sfxPlayer.play()
     }
 }
 
 #Preview {
     GamePlay()
+        .environment(Game())
 }
