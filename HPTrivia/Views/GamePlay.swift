8960b0ed384bc7038ef139a3d7cf61fc77e51bae
Masume <cetinmasume@gmail.com>
2025-08-18 17:15:48 +0300

    add : hint on gameplay - rotation

--- HPTrivia/Views/GamePlay.swift
+++ HPTrivia/Views/GamePlay.swift
@@ -12,6 +12,7 @@ struct GamePlay: View {
     @State private var musicPlayer : AVAudioPlayer!
     @State private var sfxPlayer : AVAudioPlayer!
     @State private var animateViewsIn :Bool = false
+    @State private var revealHint :Bool = false
     @Environment(Game.self) private var game
     @Environment(\.dismiss) private var dismiss
     var body: some View {
@@ -51,8 +52,50 @@ struct GamePlay: View {
                     .animation(.easeInOut(duration: 2),value: animateViewsIn)
                     Spacer()
                     // MARK: hints
+                    HStack{
+                        VStack{
+                        if animateViewsIn {
+                            Image(systemName: "questionmark.app.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100)
+                                .foregroundStyle(.cyan)
+                                .padding()
+                                .transition(.offset(x:-geo.size.width/2))
+                                .phaseAnimator([false,true]) { content, phase in
+                                    content.rotationEffect(.degrees(phase ? -13 : -17))
+                                }animation: { _ in
+                                        .easeInOut(duration: 0.7)
+                                }
+                                .onTapGesture {
+                                    withAnimation(.easeOut(duration: 1)) {
+                                        revealHint = true
+                                    }
+                                    playFlipSound()
+                                    //   game.currentQuestion
+                                    game.questionScore -= 1
+                                }
+                                .rotation3DEffect(.degrees(revealHint ? 1440 : 0), axis: (x: 0, y: 1, z: 0))
+                                .scaleEffect(revealHint ? 5 : 1)
+                                .offset(x :revealHint ? geo.size.width/2 : 0)
+                                .opacity(revealHint ? 0 : 1)
+                                .overlay {
+                                    Text(game.currentQuestion.hint)
+                                        .padding(.leading , 20)
+                                        .minimumScaleFactor(0.2)
+                                        .multilineTextAlignment(.center)
+                                        .opacity(revealHint ? 1 : 0)
+                                        .scaleEffect(revealHint ? 1.33 : 1)
+                                }
+                        }
+                        }.animation(.easeOut(duration: 1.5), value: animateViewsIn)
+                        Spacer()
+                    }
+                    .padding()
+                    
                     
                     // MARK: answers
+                    Spacer()
                 }
                 .frame(width: geo.size.width,height: geo.size.height)
                 .foregroundStyle(.white)
@@ -72,13 +115,13 @@ struct GamePlay: View {
         }
     }
     private func playMusic() {
-        let songs = ["let-the-mistery-unfold","spellcraft","hiding-place-in-the-forest","deep-in-the-dell"]
+        let songs = ["let-the-mystery-unfold","spellcraft","hiding-place-in-the-forest","deep-in-the-dell"]
         let song = songs.randomElement()!
         let sound = Bundle.main.path(forResource:song,ofType:"mp3")
         musicPlayer = try! AVAudioPlayer(contentsOf:URL(filePath: sound!))
         musicPlayer.numberOfLoops = -1
         musicPlayer.volume = 0.1
-        musicPlayer.play()
+       // musicPlayer.play()
     }
     private func playFlipSound() {
         let sound = Bundle.main.path(forResource:"page-flip",ofType:"mp3")
