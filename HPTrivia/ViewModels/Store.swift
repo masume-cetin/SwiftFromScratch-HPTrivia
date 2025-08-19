2b8bdae437bf1ea1ea52e9f84d543b6edce29eef
Masume <cetinmasume@gmail.com>
2025-08-19 17:04:29 +0300

    finish : store view model - watch updates of app store

--- HPTrivia/ViewModels/Store.swift
+++ HPTrivia/ViewModels/Store.swift
@@ -14,7 +14,9 @@ class Store {
     var products : [Product] = []
     var purchased  = Set<String>()
     private var updates : Task<Void,Never>? = nil
-    
+    init() {
+        updates = watchForUpdates()
+    }
     // Load available producys
     func loadProducts() async{
         do {
@@ -57,6 +59,30 @@ class Store {
         }
     }
     // check for purchased products
-    
+    private func checkPurchased() async
+    {
+        for product in products {
+            guard let status = await product.currentEntitlement else { continue }
+            
+            switch status {
+            case .unverified(let signedType, let verificationError):
+                print("error on \(signedType) : \(verificationError)")
+            case .verified(let signedType):
+                if signedType.revocationDate == nil {
+                    purchased.insert(signedType.productID)
+                }
+                else {
+                    purchased.remove(signedType.productID)
+                }
+            }
+        }
+    }
     // connect with app store to watch for purchase and transaction updates
+    private func watchForUpdates() -> Task<Void,Never> {
+        Task(priority: .background) {
+            for await _ in Transaction.updates {
+                await checkPurchased()
+            }
+        }
+    }
 }
